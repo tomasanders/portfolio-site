dce8b262dc47b828fa3963dd23d35ff0e9730255
Tom Sanders <thomas.a.sanders@gmail.com>
2022-12-03 14:58:09 +0900

    Created intro component

---
+++ components/Intro.jsx
@@ -0,0 +1,11 @@
+export const Intro = () => {
+  return (
+    <div className="intro">
+      <p>
+        Hi, I'm <span>Tom Sanders</span>,
+        <br></br>
+        a <span>front-end</span> developer.
+      </p>
+    </div>
+  );
+};

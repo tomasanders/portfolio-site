bc7b2c541fcc3cc78e22d428b77f65e465a4d3f9
Tom Sanders <thomas.a.sanders@gmail.com>
2022-12-03 16:32:12 +0900

    Logo and Intro done

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,11 +1,11 @@
 export const Intro = () => {
   return (
-    <div className="intro">
+    <section className="intro" id="home">
       <p>
         Hi, I'm <span>Tom Sanders</span>,
         <br></br>
         a <span>front-end</span> developer.
       </p>
-    </div>
+    </section>
   );
 };
---
+++ components/Logo.jsx
@@ -0,0 +1,7 @@
+export const Logo = () => {
+  return (
+    <div className="logo">
+      TS
+    </div>
+  );
+};

5915f0449dc2e39dfe6899c168d57357810400a0
Tom Sanders <thomas.a.sanders@gmail.com>
2022-12-05 20:49:10 +0900

    Added header

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,14 @@
+import Head from 'next/head';
+
+
 export default function Home() {
   return (
     <div className="app">
+      <Head>
+        <meta name="description" content="Personal Website for Tom Sanders" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"></link>
+      </Head>
       Page
     </div>
   )

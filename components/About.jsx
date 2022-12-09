bc8da2a589e13124175c745a808246208f7180d7
Tom Sanders <thomas.a.sanders@gmail.com>
2022-12-09 19:53:18 +0900

    Created 'About' page

---
+++ components/About.jsx
@@ -0,0 +1,18 @@
+export const About = () => {
+  return (
+    <div>
+      <p className="text-xs">
+        Hey there!
+        <br />
+        <br />
+        My name is <span className="text-purple-500">Tom Sanders</span>, and I'm a <span className="text-orange-400">front-end developer</span>. Thanks for stopping by!
+        <br />
+        <br />
+        I'm originally from the US, but have been living in Japan for the last 7 years. <span className="text-cyan-400">I'm headed to England early in 2023!</span> Super excited!
+        <br />
+        <br />
+        I'm passionate about building <span className="text-red-500">simple</span>, <span className="text-red-500">clean</span>, <span className="text-red-500">interesting</span> user experiences. Recently, I've been learning about accessibility best-practices. When I'm not mainlining coffee and writing code, you can find me exploring with my wife, reading a good book, or playing games.
+      </p>
+    </div>
+  );
+};

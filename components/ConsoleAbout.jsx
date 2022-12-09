160a3c7aeddc017f42c76307bf4963a3c5c0edb0
Tom Sanders <thomas.a.sanders@gmail.com>
2022-12-09 19:53:29 +0900

    Created 'console' states

---
+++ components/ConsoleAbout.jsx
@@ -0,0 +1,30 @@
+export const ConsoleAbout = () => {
+  return (
+    <p className="flex">
+      <span className="text-purple-500">
+        console
+      </span>
+      .
+      <span className="text-cyan-400">
+        log
+      </span>
+      <span className="text-yellow-200">
+        {'('}
+      </span>
+      <span className="text-purple-500">
+        Tom
+      </span>
+      .
+      <span className="text-cyan-400">
+        about
+      </span>
+      <span className="text-purple-500">
+        ()
+      </span>
+      <span className="text-yellow-200">
+        {')'}
+      </span>
+      ;
+    </p>
+  );
+};
---
+++ components/ConsoleContact.jsx
@@ -0,0 +1,30 @@
+export const ConsoleContact = () => {
+  return (
+    <p className="flex">
+      <span className="text-purple-500">
+        console
+      </span>
+      .
+      <span className="text-cyan-400">
+        log
+      </span>
+      <span className="text-yellow-200">
+        {'('}
+      </span>
+      <span className="text-purple-500">
+        Tom
+      </span>
+      .
+      <span className="text-cyan-400">
+        contact
+      </span>
+      <span className="text-purple-500">
+        ()
+      </span>
+      <span className="text-yellow-200">
+        {')'}
+      </span>
+      ;
+    </p>
+  );
+};

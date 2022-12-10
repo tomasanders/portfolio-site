94bc1ae4769cb180c9fc4ca1ee679ba1980dc499
Tom Sanders <thomas.a.sanders@gmail.com>
2022-12-10 12:01:44 +0900

    refactor Projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,87 +1,58 @@
 export const Projects = () => {
+  const projects = [
+    {
+      id: "hika",
+      name: "HiKa QUIZ",
+      type: "personal",
+      desc: "a flashcard-style hiragana and katakana quiz",
+      link: "https://github.com/tomasanders/hikaquiz",
+      tech: "Next.js, TypeScript, SASS",
+    },
+    {
+      id: "portfolio",
+      name: "Portfolio Site",
+      type: "personal",
+      desc: "this website! a fun way to show off some cool stuff I can do",
+      link: "https://github.com/tomasanders/portfolio-site",
+      tech: "Next.js, React, Tailwind CSS",
+    },
+    {
+      id: "bunpro",
+      name: "Bunpro SRS",
+      type: "professional work",
+      desc: "a comprehensive japanese language learning tool that makes learning simple and efficient",
+      link: "https://bunpro.jp",
+      tech: "jQuery, SASS, Ruby on Rails, React",
+    },
+  ];
+
   return (
     <div className="text-xs">
-      <span className="y">const</span>
-      <span className="p pl-1">projects</span>
-      <span className="pl-1">=</span>
-      <span className="y pl-1">{'{'}</span>
+      <span className="y">const</span> <span className="p">projects</span> = <span className="y">{'{'}</span>
       <br/>
-      {/* HiKa Quiz */}
-      <span className="p pl-4">hika</span>:
-      <span className="p pl-1">{'{'}</span>
-      <br/>
-      <span className="p pl-6">name</span>:
-      <span className="o pl-1">"HiKa QUIZ"</span>,
-      <br/>
-      <span className="p pl-6">type</span>:
-      <span className="o pl-1">"personal"</span>,
-      <br/>
-      <span className="p pl-6">desc</span>:
-      <span className="o pl-1">"a simple flashcard-style hiragana/katakana quiz"</span>,
-      <br/>
-      <span className="p pl-6">link</span>:
-      <span className="o pl-1">
-        <a href="https://github.com/tomasanders/hikaquiz" target="_blank" className="underline decoration-dotted underline-offset-2">"Here"</a>
-      </span>,
-      <br/>
-      <span className="p pl-6">tech</span>:
-      <span className="o pl-1">"Next.js, TypeScript, SASS"</span>,
-      <br/>
-      <span className="p pl-4">{'}'}</span>,
-      <br/>
-      <br/>
-
-      {/* Portfolio */}
-      <span className="p pl-4">portfolio</span>:
-      <span className="p pl-1">{'{'}</span>
-      <br/>
-      <span className="p pl-6">name</span>:
-      <span className="o pl-1">"Portfolio Site"</span>,
-      <br/>
-      <span className="p pl-6">type</span>:
-      <span className="o pl-1">"personal"</span>,
-      <br/>
-      <span className="p pl-6">desc</span>:
-      <span className="o pl-1">"this website! a fun way to show off some cool stuff I can do"</span>,
-      <br/>
-      <span className="p pl-6">link</span>:
-      <span className="o pl-1">
-        <a href="https://github.com/tomasanders/portfolio-site" target="_blank" className="underline decoration-dotted underline-offset-2">"Here"</a>
-      </span>,
-      <br/>
-      <span className="p pl-6">tech</span>:
-      <span className="o pl-1">"Next.js, React, Tailwind CSS"</span>,
-      <br/>
-      <span className="p pl-4">{'}'}</span>,
-      <br/>
-      <br/>
-
-      {/* Bunpro SRS */}
-      <span className="p pl-4">bunpro</span>:
-      <span className="p pl-1">{'{'}</span>
-      <br/>
-      <span className="p pl-6">name</span>:
-      <span className="o pl-1">"Bunpro SRS"</span>,
-      <br/>
-      <span className="p pl-6">type</span>:
-      <span className="o pl-1">"professional"</span>,
-      <br/>
-      <span className="p pl-6">desc</span>:
-      <span className="o pl-1">"a complete japanese language learning tool that uses grammar/vocab lessons and reviews, reading passages, audio clips, and quizzes to simplify learning japanese"</span>,
-      <br/>
-      <span className="p pl-6">link</span>:
-      <span className="o pl-1">
-        <a href="https://bunpro.jp" target="_blank" className="underline decoration-dotted underline-offset-2">"Here"</a>
-      </span>,
-      <br/>
-      <span className="p pl-6">tech</span>:
-      <span className="o pl-1">"jQuery, SASS, Ruby on Rails, React"</span>,
-      <br/>
-      <span className="p pl-4">{'}'}</span>,
-      <br/>
-
-      <span className="p pl-4">disclaimer</span>:
-      <span className="o pl-1">"more info on each project in my resume!</span>
+      {console.log(projects)}
+      {projects.map((project, index) => {
+        return (
+          <div key={index}>
+            &nbsp; <span className="p">{project.id}</span>: <span className="p">{'{'}</span>
+            <br/>
+            &nbsp; &nbsp; <span className="p">name</span>: <span className="o">"{project.name}"</span>,
+            <br/>
+            &nbsp; &nbsp; <span className="p">type</span>: <span className="o">"{project.type}"</span>,
+            <br/>
+            &nbsp; &nbsp; <span className="p">desc</span>: <span className="o">"{project.desc}"</span>,
+            <br/>
+            &nbsp; &nbsp; <span className="p">link</span>: <a href={project.link} target="_blank" className="underline decoration-dotted underline-offset-2 o">"Here"</a>,
+            <br/>
+            &nbsp; &nbsp; <span className="p">tech</span>: <span className="o">"{project.tech}"</span>,
+            <br/>
+            &nbsp; <span className="p">{'}'}</span>,
+            <br/>
+            <br/>
+          </div>
+        );
+      })}
+      &nbsp; <span className="p">disclaimer</span>: <span className="b">"more info in my resume!</span>
     </div>
   );
 };

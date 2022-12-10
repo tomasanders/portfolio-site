ab8ed61d61a61e55785b83d101bda7e504755ffb
Tom Sanders <thomas.a.sanders@gmail.com>
2022-12-10 20:12:21 +0900

    Added <header> and <h1> for WCAG

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,14 +11,14 @@ export default function Home() {
         <meta name="author" content="Tom Sanders" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <main className="bg-app-bg h-screen w-screen flex items-center justify-center p-4">
+      <main className="flex items-center justify-center w-screen h-screen p-4 bg-app-bg">
         <div className="[console] w-full h-full bg-console-bg flex flex-col rounded-lg">
-          <div className="[buttons] flex items-start justify-start gap-2 p-2">
-            <div className="rounded-full bg-red-500 h-3 w-3"></div>
-            <div className="rounded-full bg-yellow-400 h-3 w-3"></div>
-            <div className="rounded-full bg-green-400 h-3 w-3"></div>
-            <p className="grow text-right text-slate-300 text-xs">tom_sanders--portfolio.jsx</p>
-          </div>
+          <header className="[buttons] flex items-start justify-start gap-2 p-2">
+            <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+            <div className="w-3 h-3 bg-yellow-400 rounded-full"></div>
+            <div className="w-3 h-3 bg-green-400 rounded-full"></div>
+            <h1 className="text-xs text-right grow text-slate-300">tom_sanders--portfolio.jsx</h1>
+          </header>
           <div className="[screen] px-4 max-h-[90vh] overflow-scroll pb-4 bg-dev-bg pt-2 grow">
             <Developer />
           </div>

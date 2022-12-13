2862bdbd4cfb686530a03eb0fb67e9356e2d922d
Tom Sanders <thomas.a.sanders@gmail.com>
2022-12-13 11:02:07 +0900

    Moved buttons to component

---
+++ components/ButtonBar.jsx
@@ -0,0 +1,17 @@
+export const ButtonBar = (props) => {
+  return (
+    <>
+      <div className="flex items-start justify-start gap-2 px-4 py-2 text-xs text-right bg-console-bg text-console-gray">
+        <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+        <div className="w-3 h-3 bg-yellow-400 rounded-full"></div>
+        <div className="w-3 h-3 bg-green-400 rounded-full"></div>
+        {props.head === true &&
+          <h1 className="grow">tom_sanders--portfolio.jsx</h1>
+        }
+        {props.head === false &&
+          <p className="grow">{`Console: ${props.pageName}`}</p>
+        }
+      </div>
+    </>
+  );
+};
